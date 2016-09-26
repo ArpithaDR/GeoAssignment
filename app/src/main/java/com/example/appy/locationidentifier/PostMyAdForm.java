cf378116f956468ceff7dcdd959af14b45ad41cf
Arora <iarora@usc.edu>
2016-09-26 00:00:35 -0700

    Post Ad Screen

---
+++ app/src/main/java/com/example/appy/locationidentifier/PostMyAdForm.java
@@ -0,0 +1,13 @@
+package com.example.appy.locationidentifier;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostMyAdForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_my_ad_form);
+    }
+}
