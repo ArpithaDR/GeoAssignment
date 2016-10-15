c43c87348f48635544e13c59722306dd6981ea3c
Arora <iarora@usc.edu>
2016-10-15 13:08:14 -0700

    Connection to DB from app!

--- app/src/main/java/com/example/appy/locationidentifier/PostMyAdForm.java
+++ app/src/main/java/com/example/appy/locationidentifier/PostMyAdForm.java
@@ -1,13 +1,24 @@
 package com.example.appy.locationidentifier;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -16,6 +27,7 @@ public class PostMyAdForm extends AppCompatActivity {
 
     public static final String TAG = PostMyAdForm
             .class.getSimpleName();
+    private ProgressDialog progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +51,7 @@ public class PostMyAdForm extends AppCompatActivity {
         EditText startdate_et = (EditText) findViewById(R.id.post_ad_start_date);
         EditText enddate_et = (EditText) findViewById(R.id.post_ad_end_date);
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        Date startdate, enddate;
+        Date startdate = new Date(), enddate = new Date();
         try {
             startdate = df.parse(startdate_et.getText().toString());
             enddate = df.parse(enddate_et.getText().toString());
@@ -66,6 +78,72 @@ public class PostMyAdForm extends AppCompatActivity {
                 );
 
         Toast.makeText(getBaseContext(), "Submitted!" , Toast.LENGTH_SHORT ).show();
+        String s = "http://ec2-52-53-202-11.us-west-1.compute.amazonaws.com:8080/addAddress?subject=" + subject +
+                "&address=" + address + "&price=" + price +
+                "&vacancies=" + vacancy + "&start_date="+ startdate + "&end_date=" + enddate +
+                "&description=" + description + " &phone_number=" + phone;
+        new GetRequestDemo(this).execute(s);
+    }
+
+    private class GetRequestDemo extends AsyncTask<String, Void, Void> {
+        private final Context context;
+
+        public GetRequestDemo(Context c) {
+            this.context = c;
+        }
+
+        protected void onPreExecute() {
+            progress = new ProgressDialog(this.context);
+            progress.setMessage("Loading");
+            progress.show();
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                // doesn't accept # in url-params
+                System.out.println("url:" + params[0]);
+//                String encodedUrl = URLEncoder.encode(params[0], "UTF-8");
+                URL url = new URL(params[0]);
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+
+                int responseCode = connection.getResponseCode();
+
+                System.out.println("\nSending 'GET' request to URL : " + url);
+                System.out.println("Response Code : " + responseCode);
+
+                final StringBuilder output = new StringBuilder("Response: ");
+                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line = "";
+                StringBuilder responseOutput = new StringBuilder();
+                while ((line = br.readLine()) != null) {
+                    responseOutput.append(line);
+                }
+                br.close();
+
+                System.out.println(responseOutput.toString());
+                output.append("Response msg: " + responseOutput.toString());
+
+                PostMyAdForm.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Toast.makeText(getBaseContext(), output , Toast.LENGTH_SHORT ).show();
+                        progress.dismiss();
+                    }
+                });
+
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return null;
+        }
 
     }
 }
