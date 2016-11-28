1d598d847848e794e005856788f98714cf58e57c
ArpithaDR <arpithadr18@gmail.com>
2016-11-28 15:01:09 -0800

    fixed image issue

--- app/src/main/java/com/example/appy/locationidentifier/AdDetails.java
+++ app/src/main/java/com/example/appy/locationidentifier/AdDetails.java
@@ -1,11 +1,23 @@
 package com.example.appy.locationidentifier;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.appy.utility.AsyncResponse;
+import com.example.appy.utility.HttpConnection;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class AdDetails extends AppCompatActivity {
 
+    public String strBase64 = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,5 +43,42 @@ public class AdDetails extends AppCompatActivity {
         spots.setText("Spots Available: " + bundle.getString("Spots"));
         subject.setText("Title: " + bundle.getString("Subject"));
         email.setText("Email: " + bundle.getString("Email"));
+
+        System.out.println("HouseId - " + bundle.getInt("HouseId"));
+
+        String house_id = String.valueOf(bundle.getInt("HouseId"));
+
+        String s = "http://ec2-52-53-202-11.us-west-1.compute.amazonaws.com:8080/getImage?house_id=" + house_id;
+
+        HttpConnection httpConnection = new HttpConnection(AdDetails.this, new AsyncResponse() {
+            @Override
+            public void processFinish(Object output) {
+                String result = (String) output;
+
+                JSONObject encodedImage = null;
+                try {
+                    encodedImage = new JSONObject(result);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Log.v("image: ", encodedImage.toString());
+                try {
+                    encodedImage = encodedImage.getJSONObject("image");
+                    strBase64 = encodedImage.getString("image_data").replace("\n","");
+                    System.out.println(strBase64);
+
+                    byte[] decodedString = Base64.decode(strBase64, 0);
+                    Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                    ImageView testEncoded = (ImageView) findViewById(R.id.imageView2);
+                    Bitmap scaled = Bitmap.createScaledBitmap(decodedByte, 720, 720, true);
+                    testEncoded.setImageBitmap(scaled);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        httpConnection.execute(s);
+
     }
 }
